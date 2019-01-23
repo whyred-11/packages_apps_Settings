Havoc-OS/android_packages_apps_Settings

Add activity to launch headsup settings fragment

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -138,6 +138,7 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.havoc.settings.HavocSettings;
+import com.havoc.settings.fragments.HeadsUpSettings;
 
 public class SettingsGateway {
 
@@ -263,7 +264,8 @@ public class SettingsGateway {
             DirectoryAccessDetails.class.getName(),
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
-            HavocSettings.class.getName()
+            HavocSettings.class.getName(),
+            HeadsUpSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

whyred-11/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -139,6 +139,7 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.android.settings.custom.ambient.history.AmbientPlayHistory;
 import com.ion.ionizer.IonIonizer;
+import com.ion.ionizer.fragments.HeadsUpSettings;
 
 public class SettingsGateway {
 
@@ -267,6 +268,7 @@ public class SettingsGateway {
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             AmbientPlayHistory.class.getName(),
             IonIonizer.class.getName(),
+            HeadsUpSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {