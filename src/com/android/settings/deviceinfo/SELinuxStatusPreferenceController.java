e216907f85328cbfa7d3be305dc84ac916135a51
AnkitGourav <Ankitgourav035@gmail.com>
2019-02-21 22:38:37 +0530

    fix-up

--- src/com/android/settings/deviceinfo/SELinuxStatusPreferenceController.java
+++ src/com/android/settings/deviceinfo/SELinuxStatusPreferenceController.java
@@ -29,7 +29,6 @@ import com.android.settingslib.core.AbstractPreferenceController;
 public class SELinuxStatusPreferenceController extends AbstractPreferenceController implements
         PreferenceControllerMixin {
 
-    private static final String PROPERTY_SELINUX_STATUS = "ro.build.selinux";
     private static final String KEY_SELINUX_STATUS = "selinux_status";
 
     public SELinuxStatusPreferenceController(Context context) {
@@ -38,7 +37,7 @@ public class SELinuxStatusPreferenceController extends AbstractPreferenceControl
 
     @Override
     public boolean isAvailable() {
-        return !TextUtils.isEmpty(SystemProperties.get(PROPERTY_SELINUX_STATUS));
+        return true;
     }
 
     @Override
