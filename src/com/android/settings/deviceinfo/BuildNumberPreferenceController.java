089869ec774ff1934d6f8e4d7e6288f66a41ca74
AnkitGourav <Ankitgourav035@gmail.com>
2019-03-17 16:17:27 +0530

    Append ion version/build date/time on build display ID

--- src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -78,10 +78,10 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
     public CharSequence getSummary() {
         StringBuilder sb = new StringBuilder();
         sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
-        String pixelExperienceVersion = VersionUtils.getPixelExperienceVersion();
-        if (!pixelExperienceVersion.equals("")){
+        String ionVersion = VersionUtils.getIonVersion();
+        if (!ionVersion.equals("")){
             sb.append("\n");
-            sb.append(pixelExperienceVersion);
+            sb.append(ionVersion);
         }
         return sb.toString();
     }
--- src/com/android/settings/deviceinfo/VersionUtils.java
+++ src/com/android/settings/deviceinfo/VersionUtils.java
@@ -4,7 +4,10 @@ package com.android.settings.deviceinfo;
 import android.os.SystemProperties;
 
 public class VersionUtils {
-    public static String getPixelExperienceVersion(){
-        return SystemProperties.get("org.pixelexperience.version.display","");
+    public static String getIonVersion(){
+        String buildDate = SystemProperties.get("ro.ion.build_date","");
+        String buildIon = SystemProperties.get("ro.ionizer","");
+        String buildType = SystemProperties.get("ro.ion.build_type","unofficial")/*.toUpperCase()*/;
+        return buildDate.equals("") ? "" : "ion-" + buildIon + "-" + buildDate + "-" + buildType;
     }
 }
