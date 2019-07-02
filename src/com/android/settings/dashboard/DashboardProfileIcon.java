syberia-project/platform_packages_apps_Settings

Settings UI: Open UserSettings fragment on user icon click
Change-Id: I24af7981ae6b360c445a5dec181c8cdf2bdedfb2
Signed-off-by: DennySPB <dennyspb@gmail.com>

src/com/android/settings/dashboard/DashboardProfileIcon.java
@@ -5,6 +5,7 @@ import android.os.UserManager;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -12,17 +13,22 @@ import android.content.pm.UserInfo;
 
 import com.android.settings.R;
 import com.android.settingslib.Utils;
+import com.android.settings.core.SubSettingLauncher;
+import com.android.settings.users.UserSettings;
 
-public class DashboardProfileIcon extends RelativeLayout {
+import com.android.internal.logging.nano.MetricsProto;
+
+public class DashboardProfileIcon extends RelativeLayout implements OnClickListener {
+
+    private View mUsers;
 
     public DashboardProfileIcon(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(context);
     }
-	
-	public DashboardProfileIcon(Context context) {
+        public DashboardProfileIcon(Context context) {
         super(context);
-		init(context);
+        init(context);
     }
 
     private void init(Context context) {
@@ -35,5 +41,17 @@ public class DashboardProfileIcon extends RelativeLayout {
         setLayoutParams(lp);
         name.setText(info.name);
         icon.setImageDrawable(Utils.getUserIcon(context, userManager, info));
+        mUsers = findViewById(R.id.dashboard_profile_icon);
+        mUsers.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mUsers) {
+            new SubSettingLauncher(getContext())
+                    .setDestination(UserSettings.class.getName())
+                    .setSourceMetricsCategory(MetricsProto.MetricsEvent.SYBERIA)
+                    .launch();
+        }
     }
 }

whyred-11/packages_apps_Settings

src/com/android/settings/dashboard/DashboardProfileIcon.java
@@ -5,6 +5,7 @@ import android.os.UserManager;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -12,17 +13,22 @@ import android.content.pm.UserInfo;
 
 import com.android.settings.R;
 import com.android.settingslib.Utils;
+import com.android.settings.core.SubSettingLauncher;
+import com.android.settings.users.UserSettings;
 
-public class DashboardProfileIcon extends RelativeLayout {
+import com.android.internal.logging.nano.MetricsProto;
+
+public class DashboardProfileIcon extends RelativeLayout implements OnClickListener {
+
+    private View mUsers;
 
     public DashboardProfileIcon(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(context);
     }
-	
-	public DashboardProfileIcon(Context context) {
+        public DashboardProfileIcon(Context context) {
         super(context);
-		init(context);
+        init(context);
     }
 
     private void init(Context context) {
@@ -35,5 +41,17 @@ public class DashboardProfileIcon extends RelativeLayout {
         setLayoutParams(lp);
         name.setText(info.name);
         icon.setImageDrawable(Utils.getUserIcon(context, userManager, info));
+        mUsers = findViewById(R.id.dashboard_profile_icon);
+        mUsers.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mUsers) {
+            new SubSettingLauncher(getContext())
+                    .setDestination(UserSettings.class.getName())
+                    .setSourceMetricsCategory(MetricsProto.MetricsEvent.ION_IONIZER)
+                    .launch();
+        }
     }
 }